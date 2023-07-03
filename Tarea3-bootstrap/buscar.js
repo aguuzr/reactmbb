2f94e55e83a22b5e2a255a44e9159f1a3797b6a0
aguuzr <63855250+aguuzr@users.noreply.github.com>
2023-07-03 10:32:08 -0300

    Add files via upload

--- Tarea3-bootstrap/buscar.js
+++ Tarea3-bootstrap/buscar.js
@@ -2,25 +2,25 @@ function buscar() {
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=shrek')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         document.getElementById("card1").src = data.Poster;
     });
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=luca')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         document.getElementById("card2").src = data.Poster;
     });
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=encanto')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         document.getElementById("card3").src = data.Poster;
     });
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=inside+out')
     .then(response => response.json())
     .then(data => {
-        console.log(data);
+        //console.log(data);
         document.getElementById("card4").src = data.Poster;
     });
 }
