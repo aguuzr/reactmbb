ed9d261d2491c57e3c110c8586448f7b4a891727
aguuzr <63855250+aguuzr@users.noreply.github.com>
2023-07-03 12:43:13 -0300

    Add files via upload

--- Tarea3-bootstrap/buscar.js
+++ Tarea3-bootstrap/buscar.js
@@ -1,26 +1,28 @@
+let peliculas = ['shrek','luca','encanto','inside+out'];
+
 function buscar() {
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=shrek')
     .then(response => response.json())
     .then(data => {
         //console.log(data);
-        document.getElementById("card1").src = data.Poster;
-    });
+        document.getElementById("card0").src = data.Poster;
+    });    
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=luca')
     .then(response => response.json())
     .then(data => {
         //console.log(data);
-        document.getElementById("card2").src = data.Poster;
+        document.getElementById("card1").src = data.Poster;
     });
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=encanto')
     .then(response => response.json())
     .then(data => {
         //console.log(data);
-        document.getElementById("card3").src = data.Poster;
+        document.getElementById("card2").src = data.Poster;
     });
     fetch('http://www.omdbapi.com/?apikey=4ee84151&t=inside+out')
     .then(response => response.json())
     .then(data => {
         //console.log(data);
-        document.getElementById("card4").src = data.Poster;
+        document.getElementById("card3").src = data.Poster;
     });
 }
